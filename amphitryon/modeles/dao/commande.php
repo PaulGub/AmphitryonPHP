e0d5637aaedf2c77745f0ea3aad69afdb73a9ffe
Paul Gubbiotti <90463998+PaulGub@users.noreply.github.com>
2022-05-10 11:26:31 +0200

    Delete amphitryon/modeles/dao directory

--- amphitryon/modeles/dao/commande.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'param.php';
-require_once 'UserDAO.php';
-
-print(json_encode(UserDAO::commande($_POST['table'])));
--- amphitryon/modeles/dao/commandePlat.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'param.php';
-require_once 'UserDAO.php';
-
-print(json_encode(UserDAO::commandePlat($_POST['plat'],$_POST['quantitee'],$_POST['infos'])));
